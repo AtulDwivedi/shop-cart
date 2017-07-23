3ea17f5fbd42a278fb498b621545329a6a2c644c
preyosipal <preyosipal@gmail.com>
2017-07-23 11:42:07 +0530

    Remove comments from OrderServlet.java

--- src/com/shopcart/controller/OrderServlet.java
+++ src/com/shopcart/controller/OrderServlet.java
@@ -19,14 +19,14 @@ public class OrderServlet extends HttpServlet {
      */
     public OrderServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
