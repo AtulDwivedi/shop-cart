430e1f565360d2e24fcb23d161b4ff3462d6f3b2
PreetiKumari21 <preeti4recruit@gmail.com>
2017-07-23 12:12:44 +0530

    Removed the comment

--- src/com/shopcart/controller/OrderServlet.java
+++ src/com/shopcart/controller/OrderServlet.java
@@ -34,7 +34,7 @@ public class OrderServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
